a5bb3399a96985b684903b3ebfc1941567707937
Nikit4v <57871541+Nikit4v@users.noreply.github.com>
2021-08-04 13:31:03 +0300

    Initial commit

---
+++ AssaultEngine/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AssaultEngine
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
